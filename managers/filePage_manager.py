8579a6d57b98a12ebce65d8e20071c329162947f
dihia <dihiaslimana14@gmail.com>
2024-12-01 15:25:33 +0100

    add sftp manager

--- managers/filePage_manager.py
+++ managers/filePage_manager.py
@@ -1,4 +1,5 @@
 import os
+import paramiko
 from werkzeug.utils import secure_filename
 
 # Dossier pour stocker les fichiers
@@ -12,8 +13,32 @@ def get_user_folder(user_id):
     os.makedirs(user_folder, exist_ok=True)  # Crée le dossier si n'existe pas
     return user_folder
 
+#+++++++++++++++++++++++++ Fonction pour se connecter au serveur SFTP  +++++++++++++++++++++++++++++++++++
 
-def upload_file(request, user_id):
+def connect_sftp():
+    hostname = "192.168.1.17"  
+    username = "admin" 
+    private_key_path = "/home/dihia/.ssh/id_rsa_new"  
+
+    try:
+       
+        private_key = paramiko.RSAKey.from_private_key_file(private_key_path)
+        
+        # creation d'une connexion sftp
+        transport = paramiko.Transport((hostname, 22))
+        transport.connect(username=username, pkey=private_key)
+        sftp = paramiko.SFTPClient.from_transport(transport)
+        print("Connexion sftp reussie ")
+        return sftp
+    except paramiko.AuthenticationException:
+        print("Échec de l'authentification ")
+    except Exception as e:
+        print(f"Erreur lors de la connexion ... : {e}")
+    return None
+
+#+++++++++++++++++++++++++++++++++++++++ Uploads section local + SFTP +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+def upload_file(request, user_id, sftp):
     if 'files' not in request.files:
         return {"error": "No files part"}, 400
 
@@ -35,19 +60,55 @@ def upload_file(request, user_id):
             file.save(file_path)
             uploaded_files.append(filename)
 
+            #
+            # Upload sur le serveur SFTP
+            if sftp:
+                user_folder = f'/uploads/{user_id}'
+                try:
+                    
+                    try:
+                        sftp.stat(user_folder)
+                    except FileNotFoundError: 
+                        sftp.mkdir(user_folder) # le creer si il existe pas !
+                    
+                    file_path = os.path.join(user_folder, filename)
+                    sftp.put(file_path, file_path)  # Upload sur SFTP
+                    print(f"Fichier uploadé sur SFTP : {file_path}")
+                except Exception as e:
+                    print(f"Erreur SFTP pour {filename}: {e}")
+
     return {"message": "Files uploaded successfully", "files": uploaded_files}, 200
 
 # Fonction pour lister les fichiers d'un utilisateur
 def list_files(user_id):
     user_folder = get_user_folder(user_id)
-    return os.listdir(user_folder)  # Renvoie la liste des fichiers dans le dossier de l'utilisateur
+    files =  os.listdir(user_folder)  # Renvoie la liste des fichiers dans le dossier de l'utilisateur
+
+    # if sftp:   //  a Voir !!
+    #     user_folder = f'/uploads/{user_id}'
+
+    return files 
 
 
-def delete_file(user_id, file_name):
-    user_folder = get_user_folder(user_id)
-    file_path = os.path.join(user_folder, file_name)
-    if os.path.exists(file_path):
-        os.remove(file_path)
+# ++++++++++++++++++++++++++++++++++++++++++Delete files local + SFTP ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+def delete_file(user_id, file_name, sftp=None):
+    local_user_folder = get_user_folder(user_id)
+    local_file_path = os.path.join(local_user_folder, file_name)
+
+    if os.path.exists(local_file_path):
+        os.remove(local_file_path)
+
+        if sftp:
+                user_folder = f'/uploads/{user_id}'
+                file_path = os.path.join(user_folder, file_name)
+                try:
+                    sftp.remove(file_path)  # Supprimer le fichier distant
+                    print(f"Fichier supprimé de SFTP : {file_path}")
+                except Exception as e:
+                    print(f"Erreur lors de la suppression du fichier distant : {e}")
+
+
         return {"message": "File deleted successfully"}, 200
     else:
         return {"error": "File not found"}, 404
--- routes/file.py
+++ routes/file.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, session, redirect, url_for, render_template, abort, send_from_directory
 import os
-from managers.filePage_manager import list_files, upload_file, delete_file
+from managers.filePage_manager import *
 from managers.database_manager import UserDatabase
 from init_app import db_manager, mail_manager, ip_manager  # Managers initiaux
 
@@ -16,6 +16,7 @@ def file_page(username):
     """
     user_info = db_manager.get_user_infos(username)
     user_id = user_info[0]
+
     files = list_files(str(user_id))
     print(f"USER INFO: {user_id}, FILES: {files}")
     return render_template("file_page.html", files=files, user_id=str(user_id), user_info=user_info)
@@ -26,24 +27,38 @@ def upload():
     Upload un fichier pour l'utilisateur connecté.
     """
     user_id = session.get("user_id")
+
     if not user_id:
         abort(403, description="User not authenticated.")
    
     print("in upload ")
 
+    # Connexion SFTP
+    sftp = connect_sftp()
+
     response, status_code = upload_file(request, user_id)
     if status_code == 200:
         username = session.get("username")
         user_info = db_manager.get_user_infos(username)
-        files = list_files(str(user_id))
+
+        files = list_files(str(user_id), sftp)
         response["files"] = files
+
+                # Fermer la connexion SFTP après l'upload
+        if sftp:
+            sftp.close()
+
     return response, status_code
+
 
 @file_blueprint.route("/delete", methods=["POST"])
 def delete_file_route():
     """
     Supprime un fichier pour l'utilisateur connecté.
     """
+
+    sftp = connect_sftp()
+
     user_id = session.get("user_id")
     username = session.get("username")
     if not user_id or not username:
@@ -53,8 +68,14 @@ def delete_file_route():
     if not file_name:
         abort(400, description="File name is required.")
 
-    delete_file(user_id, file_name)
+    delete_file(user_id, file_name, sftp=sftp)  #  AVOIR  !
+
+    if sftp:
+            sftp.close()
+
     return redirect(url_for("file.file_page", username=username))
+
+
 
 @file_blueprint.route("/download/<user_id>/<filename>")
 def download_file(user_id, filename):
